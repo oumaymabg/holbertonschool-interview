c82b4ef868c227de06ddb9fb0ec76e36ddfeeefe
oumaymabg <oumaymabou257@gmail.com>
2021-04-22 22:40:18 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,10 +2,12 @@
 """
 lockboxes , we have n number  of locked boxes in front of you.
  Each box is numbered sequentially from 0 to
- n - 1 and each box may contain keys to the other boxes."""
+ n - 1 and each box may contain keys to the other boxes.
+"""
+
 
 def canUnlockAll(boxes):
-    """determines if all the boxes can be opned"""
+    '''determines if all the boxes can be opned'''
     valide = 'true'
     open = [0]
     while(len(open) != len(boxes)):
@@ -16,15 +18,3 @@ def canUnlockAll(boxes):
         print ('true')
     else:
         print ('false')
-
-def AjoutOpen(open):
-    print ('test3')
-    for i in range (1, len(open) ):
-        print ('test4')
-        for j in range (1, len(boxes)):
-            print ('test5')
-            if j == open[i]:
-                print ('test6')
-                if isinstance(boxes[j], open):
-                    print ('test7')
-                    open.append(j)
