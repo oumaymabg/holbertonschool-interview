e1524535bbd3114798a2dacc2face1694f0a5353
oumaymabg <oumaymabou257@gmail.com>
2021-05-05 14:27:06 +0200

    basic binary tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,18 @@
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+ {
+     binary_tree_t node* node
+     =(binary_tree_t node*)malloc(sizeof(binary_tree_t node));
+     node->data = data;
+     node->left = Null;
+     node->right = Null;
+     return (node);
+
+ }
