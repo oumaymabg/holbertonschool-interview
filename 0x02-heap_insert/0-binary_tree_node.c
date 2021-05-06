554e1f3bbca12ab216b41f4f241b1bd81f2d5ea5
oumaymabg <oumaymabou257@gmail.com>
2021-05-06 10:47:27 +0200

    Binary_tree_node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -3,26 +3,23 @@
 #include "binary_trees.h"
 
 /**
- * struct binary_tree_s - Binary tree node
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent node
+ * @value: integer for the new node
  *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * Return: a pointer to the new node, or NULL on failure
  */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
- {
-    binary_tree_t * new_node;
-
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
 		return (NULL);
-	 new_node->n = value;
-     new_node->parent = parent;
-     new_node->left = Null;
-     new_node->right = Null;
-     
-     return (new_node);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 
- }
+	return (new);
+}
