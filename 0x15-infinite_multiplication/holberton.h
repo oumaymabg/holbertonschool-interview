6b27744fc71d36aab4d4a14d2e71b04508b1130b
oumaymabg <oumaymabou257@gmail.com>
2021-09-27 23:17:28 +0200

    Holberton

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[]);
+int _putchar(char c);
+int _strlen(char *);
+int is_digit(char *c);
+void multiply(char *s1, char *s2);
+#endif /* HOLBERTON_H */
