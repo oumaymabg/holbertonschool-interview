7a9d75156c72d3757ea31ea6f8a90891845db9ef
oumaymabg <oumaymabou257@gmail.com>
2021-09-27 02:06:32 +0200

    search_algos

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Extern libraries */
+
+int linear_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* SEARCH_H */
